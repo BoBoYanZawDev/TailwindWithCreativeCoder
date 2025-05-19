3ad64e63eb2c2facfcb43a53bd42be2bc47e6c37
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2025-05-19 16:22:01 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="nav">
+      <div class="img-container w-[90px]">
+        <img src="./assets/duo_logo.svg" alt="">
+      </div>
+      <div class="flex gap-7  ">
+          <ul class="lg:flex gap-6 items-center hidden">
+            <li><a href=""></a>Process</li>
+            <li><a href=""></a>Integrations</li>
+            <li><a href="">Integrations</a></li>
+            <li><a href="">Pricing</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Reviews</a></li>
+          </ul>
+           <button type="button" class="hidden lg:flex bg-own-primary px-4 text-white rounded-md py-1 hover:translate-y-1 hover:translate-x-[-3px] transition-all hover:bg-gray-700">Buy Template</button>
+           <svg class="lg:hidden" xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M5 5L19 19M5 19L19 5"><animate fill="freeze" attributeName="d" dur="0.4s" values="M5 5L19 19M5 19L19 5;M5 5L19 5M5 19L19 19"/></path><path d="M12 12H12" opacity="0"><animate fill="freeze" attributeName="d" begin="0.2s" dur="0.4s" values="M12 12H12;M5 12H19"/><set fill="freeze" attributeName="opacity" begin="0.2s" to="1"/></path></g></svg>
+      </div>
+  </div>
+  <section class="section1">
+    <div class="md:">
+
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class' ,
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'own-primary' : '#7971EB' ,
+        'own-border-color' : '#E6E6EF' 
+      }
+    },
+  },
+  plugins: [],
+}
+
