6276211fcc62b4ad9d50109d77d6d6f68bf266a1
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2025-05-20 20:25:24 +0630

    update

--- src/App.vue
+++ src/App.vue
@@ -16,21 +16,36 @@
            <svg class="lg:hidden" xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M5 5L19 19M5 19L19 5"><animate fill="freeze" attributeName="d" dur="0.4s" values="M5 5L19 19M5 19L19 5;M5 5L19 5M5 19L19 19"/></path><path d="M12 12H12" opacity="0"><animate fill="freeze" attributeName="d" begin="0.2s" dur="0.4s" values="M12 12H12;M5 12H19"/><set fill="freeze" attributeName="opacity" begin="0.2s" to="1"/></path></g></svg>
       </div>
   </div>
-  <section class="section1 container px-4 mx-auto grip grid-cols-2 gap-4">
-    <div class="col-span-1 flex align-center ">
-        <h1 class="font-bold text-6xl">
-          Landing Page Template for <span class="text-own-primary ">
-            Startups 
-          </span>
-        </h1>
-        <p class="text-sm text-left">Use our template to quickly and efficiently brand your growing business.</p>
-        <div class="flex gap-4">
-          <button type="button" class="btn">Preview Template</button>
-          <button type="button" class=" btn-outline ">Learn More</button>
-        </div>
+  <section class="section1 custom-padding mx-auto flex mb-10 md:py-[90px] lg:py-[40px] align-middle">
+    <div class="w-[50%] align-middle flex items-center">
+       <div >
+         <h1 class="font-bold text-6xl mb-5">
+           Landing Page Template for <span class="text-own-primary ">
+             Startups 
+           </span>
+         </h1>
+         <p class="text-lg text-left mb-4">Use our template to quickly and efficiently brand your growing business.</p>
+         <div class="flex gap-4">
+           <button type="button" class="btn">Preview Template</button>
+           <button type="button" class=" btn-outline ">Learn More</button>
+         </div>
+       </div>
       </div>
- <div class="col-span-2">
-      <img src="" alt="">
+ <div  class="w-[50%]"  >
+      <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/662a5c31b674b313da821db0_Hero-Illustration.svg" alt="">
+    </div>
+  </section>
+  <section>
+    <p class="text-center text-lg font-semibold mb-3">More than <span class="text-own-primary"> 1000+</span> Companies using our template</p>
+    <div class="flex justify-center gap-10  items-center">
+      <div>
+        <img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ab9ed663c4f53ebba1e8_DotSquare-Logo.svg" alt="">
+      </div>
+      <div><img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665abbcfc4dca184183e738_Wiggle-Logo.svg" alt=""></div>
+      <div><img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665abe3bb1cc3289b9bfd5a_Sealife-Logo.svg" alt=""></div>
+      <div><img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ac001434b3a48f948b84_LoopMedia-Logo.svg" alt=""></div>
+      <div><img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ac172b9167ca66cce90e_Tricole45-Logo.svg" alt=""></div>
+      <div><img src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ac32bb1cc3289b9c29a4_PlutoDelux-Logo.svg" alt=""></div>
     </div>
   </section>
 </template>
